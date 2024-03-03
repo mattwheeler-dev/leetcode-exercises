f1f0c33c376f1047acb6629b0dee51cd62dffc24
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2024-03-02 16:01:53 -0800

    Add Exercise: Plus One

---
+++ plus-one.js
@@ -0,0 +1,12 @@
+const plusOne = (digits) => {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        digits[i] += 1;
+        if(digits[i] > 9) {
+            digits[i] = 0;
+        } else {
+            return digits;
+        }
+    }
+    digits.unshift(1);
+    return digits;
+};
