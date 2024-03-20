5988e3dbce9650afe61c054bc5f5af0bd5ca931a
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2024-03-20 16:52:53 -0700

    Update valid-parentheses.js

--- valid-parentheses.js
+++ valid-parentheses.js
@@ -1,16 +1,21 @@
 const isValid = (parens) => {
+	// STORE CLOSING PARENS IN ARRAY
 	let closers = [];
+	// LOOP THRU GIVEN PARENS
 	for (let i = 0; i < parens.length; i++) {
+		// DETERMINE PAREN TYPE & PUSH CLOSED VERSION TO ARRAY
 		if (parens[i] == "{") {
 			closers.push("}");
 		} else if (parens[i] == "[") {
 			closers.push("]");
 		} else if (parens[i] == "(") {
 			closers.push(")");
-		}
+		} 
+		// IF CLOSING PAREN, COMPARE TO CLOSERS ARRAY TO CHECK FOR CORRECT ORDER / AMOUNT
 		else if (closers.pop() !== parens[i]) {
 			return false;
 		}
 	}
+	// RETURN IF CLOSERS ARRAY EXISTS, IT SHOULD NOT IF INPUT WAS VALID
 	return !closers.length;
 };
