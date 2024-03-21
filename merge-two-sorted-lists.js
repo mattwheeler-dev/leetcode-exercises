ff7dc00f47a85357b957274ecd5e843a1c4b63f6
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2024-03-20 17:38:58 -0700

    Update merge-two-sorted-lists.js

--- merge-two-sorted-lists.js
+++ merge-two-sorted-lists.js
@@ -1,7 +1,9 @@
 const mergeTwoLists =  (list1, list2) => {
+	// IF EITHER LIST DOES NOT EXIST, RETURN THE OTHER
 	if (!list1 || !list2) {
 		return list2 || list1;
 	}
+	// COMPARE VALS AND RETURN LOWER ONE
 	else if (list1.val <= list2.val) {
 		list1.next = mergeTwoLists(list1.next, list2);
 		return list1;
@@ -10,4 +12,5 @@ const mergeTwoLists =  (list1, list2) => {
 		list2.next = mergeTwoLists(list1, list2.next);
 		return list2;
 	}
+	// RECURSION WILL MERGE THE LISTS IN ASCENDING ORDER
 };
