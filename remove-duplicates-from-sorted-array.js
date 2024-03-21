13cddc91505cbde02c0f3d6abbdfc29170338186
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2024-03-21 14:57:04 -0700

    Update remove-duplicates-from-sorted-array.js

--- remove-duplicates-from-sorted-array.js
+++ remove-duplicates-from-sorted-array.js
@@ -1,7 +1,12 @@
 const removeDuplicates = (nums) => {
+    // STORE AMT OF UNIQUE VALS
     let k = 0;
+
+    // LOOP THRU ARRAY, COMPARE VALS, REPLACE WITH UNIQUE VALS
     nums.forEach((num) => {
         num > nums[k] && (nums[++k] = num)
     });
+
+    // RETURN AMT OF UNIQUE VALS
     return k + 1;
 };
