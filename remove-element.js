5490ded6e3123646c21ddbdf3eb9c0fd117603fe
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2024-03-21 15:17:37 -0700

    Update remove-element.js

--- remove-element.js
+++ remove-element.js
@@ -1,12 +1,21 @@
 const removeElement = (nums, val) => {
+    // NEW ARRAY TO STORE NUMS THAT != VAL
     let newNums = [];
+
+    // LOOP THRU NUMS
     for(const num of nums) {
+        // IF A NUM DOES NOT MATCH VAL, PUSH TO NEW ARRAY
         if (num != val) {
-            newNums.push(num)
-        }
-    }
+            newNums.push(num);
+        };
+    };
+
+    // LOOP THRU NUMS 
     for (let i = 0; i < nums.length; i ++) {
-        nums[i] = newNums[i]
-    }
+        // CHANGE NUMS TO CONTAIN ONLY NUMS THAT != VAL
+        nums[i] = newNums[i];
+    };
+
+    // RETURN NEW NUMS LENGTH / AMT OF NUMS != VAL
     return newNums.length;
 };
