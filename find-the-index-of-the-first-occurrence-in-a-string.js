5af78ab0bfc4807c4e9719be9d42bb213b19a0f1
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2024-03-21 15:19:28 -0700

    Update find-the-index-of-the-first-occurrence-in-a-string.js

--- find-the-index-of-the-first-occurrence-in-a-string.js
+++ find-the-index-of-the-first-occurrence-in-a-string.js
@@ -1,7 +1,10 @@
 const strStr = (haystack, needle) => {
+    // SEARCH FOR NEEDLE
     if (haystack.includes(needle)) {
+        // IF NEEDLE FOUND, RETURN INDEX
         return haystack.indexOf(needle);
     } else {
+        // NEEDLE NOT FOUND, RETURN -1
         return -1;
-    }
+    };
 };
