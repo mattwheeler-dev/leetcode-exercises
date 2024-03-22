aa041fa585860cd2f9101989a8b277bbff32f958
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2024-03-22 15:19:47 -0700

    Update length-of-last-word.js

--- length-of-last-word.js
+++ length-of-last-word.js
@@ -1,5 +1,8 @@
 const lengthOfLastWord = (s) => {
+    // TRIM ENDS AND SPLIT INTO AN ARRAY
     const words = (s.trim()).split(' ');
+    // POP LAST WORD FROM ARRAY
     const lastWord = words.pop();
+    // CREATE ARRAY FROM LAST WORD AND RETURN ITS LENGTH
     return Array.from(lastWord).length;
 };
