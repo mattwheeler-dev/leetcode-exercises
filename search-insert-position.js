0cbe390ea743b02991474bc8c8d23eb3cedb392c
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2024-03-22 15:15:52 -0700

    Update search-insert-position.js

--- search-insert-position.js
+++ search-insert-position.js
@@ -1,11 +1,17 @@
 const searchInsert = (nums, target) => {
+    // LOOP THRU NUMS ARRAY
     for (let i = 0; i <= nums.length; i++) {
+        // IF TARGET FOUND, RETURN INDEX || IF LOOPED ENTIRE ARRAY, RETURN ARRAY LENGTH (NEW ITEM INDEX IF ADDED TO THE END)
         if((nums[i] === target) || (i === nums.length)) {
             return i;
-        } else if (nums[i] < target && nums[i + 1] > target) {
+        } 
+        // IF CURRENT NUM IS < TARGET AND NEXT NUM IS > TARGET, RETURN CURRENT INDEX + 1
+        else if (nums[i] < target && nums[i + 1] > target) {
             return i + 1;
-        } else if (target < nums[0]) {
+        } 
+        // IF TARGET IS < FIRST NUM, RETURN 0
+        else if (target < nums[0]) {
             return 0;
         }
-    }
+    };
 };
