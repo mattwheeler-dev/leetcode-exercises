40dd5afae279ee57e3a7a556b3cb43ac44d8ef7c
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2024-03-23 15:35:43 -0700

    Update plus-one.js

--- plus-one.js
+++ plus-one.js
@@ -1,12 +1,22 @@
 const plusOne = (digits) => {
+    // LOOP THRU ARRAY IN REVERSE ORDER
     for (let i = digits.length - 1; i >= 0; i--) {
+        // ADD 1 TO CURRENT DIGIT
         digits[i] += 1;
+    
+        // IF CURRENT DIGIT BECOMES 10, REPLACE WITH 0
         if(digits[i] > 9) {
             digits[i] = 0;
-        } else {
+        } 
+        // ELSE RETURN CURRENT DIGIT
+        else {
             return digits;
-        }
-    }
+        };
+    };
+    
+    // INSERT 1 TO BEGINNING OF DIGITS
     digits.unshift(1);
+    
+    // RETURN NEW DIGITS
     return digits;
 };
