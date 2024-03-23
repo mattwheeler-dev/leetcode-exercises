8136c0bf851b8542783928316138df17da35b40d
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2024-03-23 16:18:32 -0700

    Update two-sum.js

--- two-sum.js
+++ two-sum.js
@@ -1,10 +1,18 @@
 const twoSum = (nums, target) => {
+	// MAP ARRAY FOR STORING AND CHECKING VALS
 	let mapArray = new Map();
+
+	// LOOP THRU NUMS
 	for (let i = 0; i < nums.length; i++) {
+		// STORE DIFF OF TARGET - CURRENT VAL 
 		let difference = target - nums[i];
+
+		// IF MAP HAS DIFF, RETURN INDEX OF DIFF AND CURRENT VAL
 		if (mapArray.has(difference)) {
 			return [mapArray.get(difference), i];
-		}
+		};
+
+		// ANSWER HAS NOT BEEN FOUND, SET VALS INTO MAP
 		mapArray.set(nums[i], i);
-	}
+	};
 };
