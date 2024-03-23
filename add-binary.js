4143b8ff6eb34cfcf8d3c8dbc99b07a085e4353c
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2024-03-23 16:04:28 -0700

    Update add-binary.js

--- add-binary.js
+++ add-binary.js
@@ -1,45 +1,38 @@
 const addBinary = (a, b) => {
-    const lenA = a.length;
-    const lenB = b.length;
-    let answer = '';
+    // STORE POSSIBLE CARRY (0 / 1)
     let carry = 0;
-    if (lenA > lenB) {
-        for (i = 0; i < lenA - lenB; i++) {
-            b = '0' + b;
-        }
-    } else if (lenA < lenB) {
-        for (i = 0; i < lenB - lenA; i++) {
-            a = '0' + a;
-        }
-    }
-    for (let i = (a.length - 1); i >= 0; i--) {
-        if (a[i] === '0' && b[i] === '0' && carry === 0) {
-            answer = "0" + answer;
-        } else if (a[i] === '0' && b[i] === '0' && carry === 1) {
-            answer = "1" + answer;
-            carry = 0;
-        } else if (a[i] === '0' && b[i] === '1' && carry === 1) {
-            answer = "0" + answer;
-            carry = 1;
-        } else if (a[i] === '1' && b[i] === '0' && carry === 1) {
-            answer = "0" + answer;
-            carry = 1;
-        } else if (a[i] === '1' && b[i] === '0' && carry === 0) {
-            answer = "1" + answer;
-            carry = 0;
-        } else if (a[i] === '1' && b[i] === '1' && carry === 1) {
-            answer = "1" + answer;
-            carry = 1;
-        } else if (a[i] === '1' && b[i] === '1' && carry === 0) {
-            answer = "0" + answer;
-            carry = 1;
-        } else if (a[i] === '0' && b[i] === '1' && carry === 0) {
-            answer = "1" + answer;
-            carry = 0;
-        }
-    }
-if (carry === 1) {
-    answer = '1' + answer;
-}
-return answer;
+    // STORE AND CREATE RESULT TO RETURN
+    let result = "";
+    // STORE LENGTH FOR LOOP; INIT AS A.LENGTH
+    let maxLength = a.length;
+
+    // IF A IS LONGER, ADD LEADING 0S TO B
+    if (a.length > b.length){
+        b = "0".repeat(a.length - b.length) + b;
+    } 
+    // ELSE MAXLENGTH IS B.LENGTH, AND ADD LEADING 0S TO A
+    else {
+        maxLength = b.length;
+        a = "0".repeat(b.length - a.length) + a;
+    };
+
+    // LOOP THRU STRINGS
+    for (let i = maxLength - 1; i >= 0; i--){
+        // PARSE TO INTS, ADD THEM, ADD CARRY, STORE IN VARIABLE
+        let sum = parseInt(a[i]) + parseInt(b[i]) + carry;
+        
+        // ADD NEW BINARY TO RESULT 
+        result = (sum % 2) + result;
+
+        // DECIDE IF CARRY IS 1 OR 0
+        carry = sum >= 2 ? 1 : 0;
+    };
+
+    // IF CARRY IS TRUE (1), ADD LEADING 1
+    if (carry){
+        result = "1" + result;
+    };
+
+    // RETURN RESULTING STRING
+    return result;
 };
