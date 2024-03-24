a1dda3b2b1cd7b20b451187358ab07e544ac6506
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2024-03-24 14:03:21 -0700

    Update longest-common-prefix.js

--- longest-common-prefix.js
+++ longest-common-prefix.js
@@ -1,8 +1,15 @@
 const longestCommonPrefix = (strs) => {
+	// IF ARRAY CONTAINS 0 OR 1 ITEM, RETURN EMPTY STRING OR THE ITEM
 	if (strs.length == 0 || strs.length == 1) {
 		return strs[0] || "";
-	}
+	};
+
+	// INCREMENT FOR LOOP
 	let i = 0;
+
+	// LOOP THRU STRINGS, COMPARING CHARACTERS
 	while (strs[0][i] && strs.every((s) => s[i] === strs[0][i])) i++;
+	
+	// RETURN STRING OF MATCHED CHARACTERS
 	return strs[0].substr(0, i);
 };
