f094ca13b4d2ae8516ae6bc4021313729fa44bb5
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2024-03-26 15:13:41 -0700

    Update merge-two-sorted-lists.js

--- merge-two-sorted-lists.js
+++ merge-two-sorted-lists.js
@@ -7,8 +7,7 @@ const mergeTwoLists =  (list1, list2) => {
 	else if (list1.val <= list2.val) {
 		list1.next = mergeTwoLists(list1.next, list2);
 		return list1;
-	}
-	else if (list2.val <= list1.val) {
+	}	else if (list2.val <= list1.val) {
 		list2.next = mergeTwoLists(list1, list2.next);
 		return list2;
 	}
