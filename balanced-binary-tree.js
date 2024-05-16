207f1d60276258701a56b17fbe6ad7c48830f90b
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2024-05-15 17:33:04 -0700

    Add Exercise: Balanced Binary Tree

---
+++ balanced-binary-tree.js
@@ -0,0 +1,8 @@
+const isBalanced = (root) => {
+    if (!root) return true;
+    let height = function(node) {
+        if (!node) return 0;
+        return 1 + Math.max(height(node.left), height(node.right));
+    } 
+    return Math.abs(height(root.left) - height(root.right)) < 2 && isBalanced(root.left) && isBalanced(root.right);
+};
