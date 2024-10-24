6e276db27d4185084cd8450c767bd03b588d9d8d
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2024-10-24 15:32:05 -0700

    Update roman-to-integer.js

--- roman-to-integer.js
+++ roman-to-integer.js
@@ -1,29 +1,21 @@
-// STORE ROMAN VALUES IN OBJECT
 const romans = {
-	I: 1,
-	V: 5,
-	X: 10,
-	L: 50,
-	C: 100,
-	D: 500,
-	M: 1000,
-};
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+}
 
-const romanToInt = (n) => {
-	// STORE TOTAL
-	let total = 0;
-	// LOOP THRU GIVEN ROMAN
-	for (i = 0; i < n.length; i++) {
-		// IF CURRENT ROMAN IS LESS THAN THE NEXT, SUBTRACT IT
-		if (romans[n[i]] < romans[n[i + 1]]) {
-			total -= romans[n[i]];
-		} 
-		// ELSE ADD TO TOTAL
-		else {
-			total += romans[n[i]];
-		}
-	}
-
-	// RETURN TOTAL AFTER LOOP IS COMPLETE
-	return total;
+const romanToInt = (s) => {
+    let result = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (romans[s[i]] < romans[s[i + 1]]) {
+            result -= romans[s[i]];
+        } else {
+            result += romans[s[i]];
+        }
+    }
+    return result;
 };
