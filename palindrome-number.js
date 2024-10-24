c562e573a3ef279cbe76d1bb2391e97438985135
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2024-10-24 15:11:30 -0700

    Update palindrome-number.js

--- palindrome-number.js
+++ palindrome-number.js
@@ -1,21 +1,12 @@
 const isPalindrome = (x) => {
-	// STORE REVERSE VERSION
-	let reverseX = 0;
-	// STORE ORIGINAL VAL
-	let num = x;
-
-	// LOOP NUM IF > 0 (ALL NEGATIVE NUMS FAIL)
-	while (num > 0) {
-		// STORE RIGHT-MOST INT
-		const int = num % 10;
-
-		// MULTIPLY REVERSE VERSION AND PLACE INT TO CREATE REVERSE VERSION OF X
-		reverseX = reverseX * 10 + int;
-
-		// REMOVE RIGHT-MOST INT FROM NUM
-		num = Math.floor(num / 10);
-	};
-
-	// RETURN TRUE / FALSE BASED ON REVERSE ORDER MATCHING ORIGINAL
-	return reverseX == x;
+    let revX = 0;
+    let tempX = x;
+    if (x < 0) {
+        return false;
+    }
+    while(tempX > 0) {
+        revX = (revX * 10) + (tempX % 10)
+        tempX = Math.floor(tempX / 10)
+    }
+    return revX == x;
 };
