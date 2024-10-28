56d60e872469bbcf4014ff11ba455a31fac9336e
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2024-10-28 15:25:25 -0700

    Add Exercise: Add Two Numbers

---
+++ add-two-numbers.js
@@ -0,0 +1,16 @@
+const addTwoNumbers = (l1, l2) => {
+    let dummy = new ListNode(0);
+    let curr = dummy;
+    let carry = 0;
+    while (l1 !== null || l2 !== null || carry !== 0) {
+        let x = l1 !== null ? l1.val : 0;
+        let y = l2 !== null ? l2.val : 0;
+        let sum = carry + x + y;
+        carry = Math.floor(sum / 10);
+        curr.next = new ListNode(sum % 10);
+        curr = curr.next;
+        if (l1 !== null) l1 = l1.next;
+        if (l2 !== null) l2 = l2.next;
+    }
+    return dummy.next;
+};
