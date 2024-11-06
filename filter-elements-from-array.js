75878b5e2170dd1e1b3f52002311306db25d0b3d
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2024-11-06 14:20:24 -0800

    Add Exercise: Filter Elements From Array

---
+++ filter-elements-from-array.js
@@ -0,0 +1,9 @@
+const filter = (arr, fn) => {
+    let filteredArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            filteredArr.push(arr[i]);
+        }
+    };
+    return filteredArr;
+};
