ff1b8a0342cb3c4bac276dccba2eea5cdd6ed39f
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2024-11-20 14:24:27 -0800

    Add Exercise: Interval Cancellation

---
+++ interval-cancellation.js
@@ -0,0 +1,10 @@
+const cancellable = (fn, args, t) => {
+    fn(...args);
+    const cancelFn = () => { 
+        clearInterval(timer);
+    };
+    const timer = setInterval(() => {
+        fn(...args);
+    }, t);
+    return cancelFn;
+};
