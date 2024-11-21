b54d730a11a00d7a32757547525294716a93a86a
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2024-11-21 14:13:33 -0800

    Add Exercise: Promise Time Limit

---
+++ promise-time-limit.js
@@ -0,0 +1,20 @@
+const timeLimit = (fn, t) => {
+  return async function(...args) {
+    return new Promise((delayresolve, reject) => {
+      const timeoutId = setTimeout(() => {
+        clearTimeout(timeoutId);
+        reject("Time Limit Exceeded");
+      }, t);
+
+      fn(...args)
+        .then((result) => {
+          clearTimeout(timeoutId);
+          delayresolve(result);
+        })
+        .catch((error) => {
+          clearTimeout(timeoutId);
+          reject(error);
+        });
+    });
+  };
+};
