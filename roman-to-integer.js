16841b451904e6bc3788acb2f9b667897f1f3e75
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2026-01-06 14:49:56 -0800

    Enhance romanToInt function with comments

--- roman-to-integer.js
+++ roman-to-integer.js
@@ -1,3 +1,4 @@
+// Store roman values in object
 const romans = {
     "I": 1,
     "V": 5,
@@ -7,10 +8,12 @@ const romans = {
     "D": 500,
     "M": 1000
 }
-
 const romanToInt = (s) => {
     let result = 0;
+    // Loop through roman numerals
     for (let i = 0; i < s.length; i++) {
+        // Check if numeral's value is more or less than the next
+        // If less than next, subtract from result. If more than next, add to result
         if (romans[s[i]] < romans[s[i + 1]]) {
             result -= romans[s[i]];
         } else {
