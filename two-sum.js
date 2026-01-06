cd933b633743d05b895679f32424626c82761767
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2026-01-06 14:31:10 -0800

    Add comments to twoSum function for clarity

--- two-sum.js
+++ two-sum.js
@@ -1,7 +1,10 @@
 const twoSum = (nums, target) => {
+    // Must return array of the 2 indices
     const result = [];
+    // Nested for loops, the inner loop starts 1 index after the outer loop so the outer loop's current number is tested with all remaining numbers
     for (let i = 0; i < nums.length - 1; i++) {
         for (let j = i + 1; j < nums.length; j++) {
+            // If solution is found, push indices to result array and return the array
             if (nums[i] + nums[j] == target) {
                 result.push(nums.indexOf(nums[i]));
                 result.push(nums.lastIndexOf(nums[j]));
