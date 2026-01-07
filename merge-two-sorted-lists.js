6485c202a8a3754f0cec0bcfd6ea495e16ccdd7a
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2026-01-07 14:20:24 -0800

    Refactor mergeTwoLists to iterative approach; Add comments for clarity and explanation;

--- merge-two-sorted-lists.js
+++ merge-two-sorted-lists.js
@@ -1,11 +1,19 @@
 const mergeTwoLists = (list1, list2) => {
-    if(!list1 || !list2) {
-        return list1 || list2;
-    } else if (list1.val <= list2.val) {
-        list1.next = mergeTwoLists(list1.next, list2);
-        return list1;
-    } else if (list2.val <= list1.val) {
-        list2.next = mergeTwoLists(list2.next, list1);
-        return list2;
-    }
+    // Temporary starting node
+    const temp = { val: -1, next: null};
+    let current = temp;
+    // While the lists both still have nodes, compare nodes and keep the lowest one
+    while (list1 && list2) {
+        if (list1.val <= list2.val) {
+            current.next = list1;
+            list1 = list1.next;
+        } else {
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next;
+    };
+    // Add the remaining nodes from whichever list is not empty
+    current.next = list1 || list2;
+    return temp.next;
 };
