912dc1d95242f9a783da912561843d41c70bc6c6
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2026-01-08 14:32:45 -0800

    Refactor removeDuplicates function for clarity;

--- remove-duplicates-from-sorted-array.js
+++ remove-duplicates-from-sorted-array.js
@@ -1,14 +1,14 @@
 const removeDuplicates = (nums) => {
-    // STORE AMT OF UNIQUE VALS
+    // Track position of last unique value found
     let k = 0;
-
-    // LOOP THRU ARRAY, COMPARE VALS, REPLACE WITH UNIQUE VALS
-    nums.forEach((num) => {
-        if (nums[k] !== num) {
-            nums[++k] = num;
-        };
-    });
-
-    // RETURN AMT OF UNIQUE VALS
+    // Start loop at index 1 because index 0 will always be unique
+    for (let i = 1; i < nums.length; i++) {
+        // If current number is different from the last, move k to next write position and store new unique value at that position
+        if (nums[i] !== nums[k]) {
+            k++;
+            nums[k] = nums[i];
+        }
+    }
+    // Add 1 to k since k is the index of the last unique value found and we need to return the amount of unique values
     return k + 1;
 };
