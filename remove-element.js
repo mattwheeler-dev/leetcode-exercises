ef7466a24077cb21e3792f16a40e31c71aae864e
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2026-01-09 13:32:19 -0800

    Refactor removeElement function for efficiency and clarity;

--- remove-element.js
+++ remove-element.js
@@ -1,21 +1,15 @@
 const removeElement = (nums, val) => {
-    // NEW ARRAY TO STORE NUMS THAT != VAL
-    let newNums = [];
-
-    // LOOP THRU NUMS
-    for(const num of nums) {
-        // IF A NUM DOES NOT MATCH VAL, PUSH TO NEW ARRAY
+    // New array to store nums != val
+    let tempNums = [];
+    for (const num of nums) {
         if (num != val) {
-            newNums.push(num);
-        };
-    };
-
-    // LOOP THRU NUMS 
-    for (let i = 0; i < nums.length; i ++) {
-        // CHANGE NUMS TO CONTAIN ONLY NUMS THAT != VAL
-        nums[i] = newNums[i];
-    };
-
-    // RETURN NEW NUMS LENGTH / AMT OF NUMS != VAL
-    return newNums.length;
+            tempNums.push(num);
+        }
+    }
+    // Replace first x items in nums with tempNums values, after those elements LC doesn't care
+    for (let i = 0; i < tempNums.length; i++) {
+        nums[i] = tempNums[i];
+    }
+    // Return amount of nums != val
+    return tempNums.length;
 };
