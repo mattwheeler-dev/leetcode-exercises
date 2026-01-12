5c5db5a488899fde790787288c3f4b4c27e6b69c
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2026-01-12 13:56:32 -0800

    Refactor searchInsert logic and remove unnecessary checks;

--- search-insert-position.js
+++ search-insert-position.js
@@ -1,17 +1,10 @@
 const searchInsert = (nums, target) => {
-    // LOOP THRU NUMS ARRAY
-    for (let i = 0; i <= nums.length; i++) {
-        // IF TARGET FOUND, RETURN INDEX || IF LOOPED ENTIRE ARRAY, RETURN ARRAY LENGTH (NEW ITEM INDEX IF ADDED TO THE END)
-        if((nums[i] === target) || (i === nums.length)) {
+    for (let i = 0; i < nums.length; i++) {
+        // If target is found, or is less than current value, return index
+        if (target <= nums[i]) {
             return i;
-        } 
-        // IF CURRENT NUM IS < TARGET AND NEXT NUM IS > TARGET, RETURN CURRENT INDEX + 1
-        else if (nums[i] < target && nums[i + 1] > target) {
-            return i + 1;
-        } 
-        // IF TARGET IS < FIRST NUM, RETURN 0
-        else if (target < nums[0]) {
-            return 0;
-        }
+        };
     };
+    // If target was never less than a value, return nums length because it would be added to the end
+    return nums.length;
 };
