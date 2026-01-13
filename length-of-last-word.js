518e2e2081136edcebd341d1a62cb1618119a4d2
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2026-01-13 13:45:06 -0800

    Refactor lengthOfLastWord function for clarity

--- length-of-last-word.js
+++ length-of-last-word.js
@@ -1,8 +1,6 @@
 const lengthOfLastWord = (s) => {
-    // TRIM ENDS AND SPLIT INTO AN ARRAY
-    const words = (s.trim()).split(' ');
-    // POP LAST WORD FROM ARRAY
-    const lastWord = words.pop();
-    // CREATE ARRAY FROM LAST WORD AND RETURN ITS LENGTH
-    return Array.from(lastWord).length;
+    // Trim whitespace from ends and split words into array
+    let words = s.trim().split(" ");
+    // Return length of last word in array
+    return words[words.length - 1].length;
 };
