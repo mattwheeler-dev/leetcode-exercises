6afd036b92e2cbf2394d258bb19486bf718609ab
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2026-01-16 14:39:53 -0800

    Refactor mySqrt function for improved readability;

--- sqrt-x.js
+++ sqrt-x.js
@@ -1,25 +1,17 @@
 const mySqrt = (x) => {
-	// LEFT = VAL USED TO CHECK MATH STARTING AT 0
-  let left = 0;
-  // RIGHT = GIVEN INT
-  let right = x;
-
-  // LOOP WHILE LEFT IS < RIGHT
-  while (left <= right) {
-    // FIND AND STORE NUM IN MIDDLE OF LEFT AND RIGHT
-    const mid = Math.floor((left + right) / 2);
-
-    // RETURN MID IF MATH SHOWS MID IS CURRENTLY NEAREST SQ ROOT INT
-    if (mid * mid <= x && (mid + 1) * (mid + 1) > x) {
-      return mid;
-    } 
-    // IF MID SQUARED IS LESS THAN X, INCREASE LEFT
-    else if (mid * mid < x) {
-      left = mid + 1;
-    } 
-    // IF MID SQUARED IS MORE THAN X, DECREASE RIGHT
-    else {
-      right = mid - 1;
-    }
-  };
+	// Store both ends of the number, 0 and x
+	let left = 0;
+	let right = x;
+	// Loop through the numbers, in range, changing as needed
+	while (left <= right) {
+		const mid = Math.floor((left + right) / 2);
+		// Return mid as square root, otherwise change the range as needed
+    	if (mid * mid <= x && (mid + 1) * (mid + 1) > x) {
+      		return mid;
+    	} else if (mid * mid < x) {
+      		left = mid + 1;
+    	} else {
+      		right = mid - 1;
+    	};
+  	};
 };
