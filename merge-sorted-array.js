923d5e8df23dac5e9668b9f7f53d0d4eef1130f7
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2026-01-22 14:16:49 -0800

    Add comments for clarity;

--- merge-sorted-array.js
+++ merge-sorted-array.js
@@ -1,9 +1,12 @@
 const merge = (nums1, m, nums2, n) => {
+    // nums1 initial length is m + n, so remove the trailing 0s and keep the "real" nums1
     nums1.splice(m,n);
+    // If nums2 exists, push all of its elements into nums1
     if (nums2 && n > 0) {
         for (let j = 0; j < n; j++) {
             nums1.push(nums2[j]);
-        }
-    }
+        };
+    };
+    // Sort all elements, default sorting would cause errors so pass a numerical comparison function
     nums1.sort((a, b) => {return a - b});
 };
