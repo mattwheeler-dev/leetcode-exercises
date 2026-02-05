66e56aba07472edbdb0e67fbdae343111acb346a
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2026-02-05 14:02:38 -0800

    Add comments to maxDepth function

--- maximum-depth-of-binary-tree.js
+++ maximum-depth-of-binary-tree.js
@@ -1,8 +1,11 @@
 const maxDepth = (root) => {
+    // Null nodes add 0 to depth
     if (!root) {
         return 0
     }
+    //Recursively find max depth of left and right subtrees
     let left = maxDepth(root.left)
     let right = maxDepth(root.right)
+    // Depth is 1 (current node) + largest between left and right depths 
     return 1 + Math.max(left,right)
 };
